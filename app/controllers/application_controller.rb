4043b534dcf6ae58870fa6346e3b03507fa2c6b2
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-09-02 12:12:05 +0300

    Pushing finals

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    '<h2>Hello <em>World</em>!</h2>
+    <p>Woddup! Woddup!This is your bwoyy Martoh Incognitoh in the mix a.k.a Daniel Obizzy ama Kayoyo Khalifa, that is Kayotti</p>
+    <br/>
+    <h2>I really love art</h2>'
+   
+  end
+
+end
